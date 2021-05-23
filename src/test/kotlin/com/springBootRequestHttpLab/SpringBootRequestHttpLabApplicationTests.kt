2004c510e7777c22a4ee3d6def3c5ddadc7ae771
Wagner Paulino <wagnersantos9778@gmail.com>
2021-05-23 14:56:31 -0300

    testes basicos

--- src/test/kotlin/com/springBootRequestHttpLab/SpringBootRequestHttpLabApplicationTests.kt
+++ src/test/kotlin/com/springBootRequestHttpLab/SpringBootRequestHttpLabApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.springBootRequestHttpLab
 
+import com.springBootRequestHttpLab.service.AgenciaGroupUseCase
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class SpringBootRequestHttpLabApplicationTests {
+class SpringBootRequestHttpLabApplicationTests(
+        @Autowired val agenciaGroupUseCase: AgenciaGroupUseCase
+) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        Assertions.assertThat(agenciaGroupUseCase).isNotNull()
+    }
 
+    @Test
+    fun isLoadingAgenciaGroup() {
+        var agenciaGroup = agenciaGroupUseCase.getAgenciaGroupByState()
+        Assertions.assertThat(agenciaGroup.size).isGreaterThan(0)
+        Assertions.assertThat(agenciaGroup.get("SP")?.size).isGreaterThan(0)
+    }
 }
